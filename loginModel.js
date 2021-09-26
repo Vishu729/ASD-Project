ff37808f16eacc1b071226ec71bc181c56063d0f
VKelvinWang <62001746+VKelvinWang@users.noreply.github.com>
2021-09-26 16:31:25 +0700

    Added registration

--- loginModel.js
+++ loginModel.js
@@ -1,4 +1,10 @@
-//Christopher JS
+//Christopher & Kelvin JS
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 // Firebase login debug.
 firebase.auth().onAuthStateChanged(function(user) {
@@ -28,8 +34,6 @@ function login(loginForm, username, password) {
         setFormMessage(loginForm, "success", "logged in");
         window.location.href = "./index.html";
     });
-
-    
 }
 
 function logout() {
@@ -59,11 +63,30 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
+//validates email
+function validateEmail(){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 //listen to submit button press
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
     
+    //opens registration tab and closes login tab
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
@@ -74,6 +97,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
         //validate login
         login(loginForm, username, password);
-       
+    });
+});
+
+//sets a check for queries
+document.querySelectorAll(".form__input").forEach(inputElement => {
+    inputElement.addEventListener("blur", e => {
+        //sets a check for phone number
+        if (e.target.id === "signupPhone" && e.target.value.length > 0 && e.target.value.length < 10) {
+            setInputError(inputElement, "Phone number must be valid");
+        }
+    });
+
+    inputElement.addEventListener("input", e => {
+        clearInputError(inputElement);
     });
 });
