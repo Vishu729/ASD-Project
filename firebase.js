cfa257cde2f81cdb0cb07c5825c08ce39b2b5965
Vishu729 <vishucom.singh0@gmail.com>
2021-10-07 17:14:12 +1100

    changed style.css to index.css connected firebase to index

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZncLkSj7vmT2gcvAFfLIb-ChTQNKhlzs",
+    authDomain: "ahmazone-9f8cb.firebaseapp.com",
+    databaseURL: "https://ahmazone-9f8cb-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ahmazone-9f8cb",
+    storageBucket: "ahmazone-9f8cb.appspot.com",
+    messagingSenderId: "39687918220",
+    appId: "1:39687918220:web:a117c0d938f14b02d321a9",
+    measurementId: "G-2HNND2SMT8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
